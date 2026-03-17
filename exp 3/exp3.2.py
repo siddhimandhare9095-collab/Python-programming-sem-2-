4651e46ea09db1da7d9ac8faf7ed2fbf474acca3
siddhimandhare9095-collab <siddhimandhare9095@gmail.com>
2026-03-17 11:20:17 +0530

    Create exp3.2.py

---
+++ exp 3/exp3.2.py
@@ -0,0 +1,18 @@
+# compute simple interest using a function
+
+"""
+Created on Mon Mar  9 15:38:18 2026
+
+@author: siddhi
+"""
+
+def simple_interest(principal, rate, time):
+  si = (principal * rate * time) / 100
+  return si
+# Taking input from the user
+p = float(input("Enter principal amount: "))
+r = float(input("Enter rate of interest: "))
+t = float(input("Enter time (in years): "))
+# Function call
+interest = simple_interest(p, r, t)
+print("Simple Interest is:", interest)
