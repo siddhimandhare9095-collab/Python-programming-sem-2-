f26783799a400f3df3d65d48f9650435ee68ba17
siddhimandhare9095-collab <siddhimandhare9095@gmail.com>
2026-03-17 11:03:34 +0530

    Create exp act2.py

---
+++ exp2/exp act2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 11:01:29 2026
+
+@author: siddhi
+"""
+# Movie ticket eligibility program
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to enter the movie.")
+else:
+    print("Sorry, you are not allowed to enter.")
