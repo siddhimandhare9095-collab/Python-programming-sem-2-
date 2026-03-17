b46035df4c43249caaa7448e53b2831cb23fafbc
siddhimandhare9095-collab <siddhimandhare9095@gmail.com>
2026-03-17 11:12:10 +0530

    Create exp act5.py

---
+++ exp2/exp act5.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 11:11:04 2026
+
+@author: siddhi 
+"""
+# Student grading system
+
+marks = float(input("Enter your marks: "))
+
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 75:
+    print("Grade: B")
+elif marks >= 50:
+    print("Grade: C")
+else:
+    print("Result: Fail")
