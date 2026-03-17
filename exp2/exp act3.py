62652acc81c0ff89a3455d10d9a0811ffd41f2be
siddhimandhare9095-collab <siddhimandhare9095@gmail.com>
2026-03-17 11:06:07 +0530

    Create exp act3.py

---
+++ exp2/exp act3.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 11:04:53 2026
+
+@author:siddhi 
+"""
+# Loan approval program
+
+salary = float(input("Enter your monthly salary (₹): "))
+
+if salary >= 25000:
+    print("Loan Approved")
+else:
+    print("Loan Rejected")
