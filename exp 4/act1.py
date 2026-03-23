68d77cf8b9ecdd9500758d5035c5e5e4bca1974d
siddhimandhare9095-collab <siddhimandhare9095@gmail.com>
2026-03-23 15:24:50 +0530

    Create act1.py

---
+++ exp 4/act1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 23 15:24:16 2026
+
+@author: siddhi
+"""
+# List of student marks
+marks = [78, 85, 92, 67, 88]
+
+# Calculate average
+average = sum(marks) / len(marks)
+
+# Find topper marks
+topper = max(marks)
+
+print("Average Marks:", average)
+print("Topper Marks:", topper)
