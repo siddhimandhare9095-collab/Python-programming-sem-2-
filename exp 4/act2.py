841cd24d47ea7c6a1fbf62445b52cc447371c85f
siddhimandhare9095-collab <siddhimandhare9095@gmail.com>
2026-03-24 09:39:06 +0530

    Create act2.py

---
+++ exp 4/act2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 24 09:37:03 2026
+
+@author: siddhi
+"""
+# List of product prices
+prices = [100, 250, 75, 50, 300]
+
+# Calculate total bill
+total_bill = sum(prices)
+
+# Display result
+print("Product Prices:", prices)
+print("Total Bill =", total_bill)
