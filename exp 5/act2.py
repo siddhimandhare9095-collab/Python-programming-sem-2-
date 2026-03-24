1240cbc49746e5b5cb2b80d62de135da05206760
siddhimandhare9095-collab <siddhimandhare9095@gmail.com>
2026-03-24 10:22:03 +0530

    Create act2.py

---
+++ exp 5/act2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 24 10:20:02 2026
+
+@author: siddhi
+"""
+# Roll numbers of two classes
+classA = {1, 2, 3, 4, 5}
+classB = {3, 4, 5, 6, 7}
+
+# Find common students
+common_students = classA.intersection(classB)
+
+# Display result
+print("Students present in both classes:", common_students)
